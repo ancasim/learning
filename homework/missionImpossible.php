7a4b5aed6d40c821c66f41b73cd5ed0ae4357777
ancasim <simcea.anca@gmail.com>
2021-12-23 16:53:36 +0200

    Homework session2 - mission impossible

---
+++ homework/missionImpossible.php
@@ -0,0 +1,8 @@
+<?php
+
+$message = "Rafher gung lbh orpbzr snzvyvne jvgu cuc shapgvbaf nf gurl ner rkgerzryl hfrshy.";
+
+$decodedMessage = str_rot13($message);
+
+echo "The decoded message is: " . $decodedMessage;
+
