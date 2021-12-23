78b55e43c8c238838a80433780772b461cd87822
ancasim <simcea.anca@gmail.com>
2021-12-23 16:55:05 +0200

    Homework session2 - santa claus

---
+++ homework/SantaClaus.php
@@ -0,0 +1,9 @@
+<?php
+
+date_default_timezone_set('Europe/Bucharest');
+
+$date = strtotime('25th December 2021');
+
+$daysTillChristmas = ($date - time()) / 60 / 60 / 24;
+
+echo "Santa Claus is coming in " . ceil($daysTillChristmas) . " days!!!";
